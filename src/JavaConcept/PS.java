69a05d0acae9b93f216a07e3f6fc324e68354f9e
Sagar <sagarlasurkar.git@gmail.com>
2025-11-20 08:37:04 +0530

    this is commit from Indian timezone guy

--- src/JavaConcept/PS.java
+++ src/JavaConcept/PS.java
@@ -6,6 +6,8 @@ public class PS {
 	public void doThis(){
 		 
 		System.out.println("I am parent class method");
+		System.out.println("I am in super class");
+		System.out.println("I am in Instagram class");
 		
 	}
 	
